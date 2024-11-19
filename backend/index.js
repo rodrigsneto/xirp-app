754f42d757b076648bd9ec75a3f830712c53bb49
Rodrigues Neto <accounts@rodrigsneto.com>
2024-11-19 15:44:07 -0300

    ajustando query e rota de usuarios

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 import express from "express"
 import empresaRoutes from "./routes/empresas.js"
-import usuariosRoutes from "./routes/empresas.js"
+import usuariosRoutes from "./routes/usuariosRoutes.js"
 import cors from "cors"
+import {db} from "./db.js"
 
 const app = express()
 
@@ -12,8 +13,15 @@ app.get("/empresas", (empresaRoutes))
 app.get("/usuarios", (usuariosRoutes))
 
 app.post("/login", (req, res) => {
-    const { email, senha } = req.body
-    res.send(email + ` e ` + senha)
+    const sql = "SELECT * FROM usuarios WHERE email = ? AND senha_hash = ?"
+    const values = [
+        req.body.email,
+        req.body.senha,
+    ]
+    db.query(sql, values, (err, data) => {
+        if(data.length == 0) return res.json("Falha com Login");
+        return res.json(data)
+    })
 })
 
 app.listen(8800)
